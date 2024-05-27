df98fde7e240017abff626c923936bb60c19e3ab
Your Name <you@example.com>
2024-05-27 12:07:16 +0700

    push add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.scss";
+import Header from "./Header";
+// import Footer from "./Footer";
+// import DarkVariantExample from "./Carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import MainContent from "./MainContent";
+// import MainAdmin from "./forAdmin/MainPageAdmin";
+// import Login from "./forUser/Login";
+// import SignUp from "./forUser/SignUp";
+// import Detail from "./forUser/ProductDetail";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        {/* <DarkVariantExample />
+        <MainContent />
+        <Footer /> */}
+        {/* <MainAdmin /> */}
+        {/* <Login /> */}
+        {/* <SignUp /> */}
+        {/* <Detail /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,45 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LogoutIcon from "@mui/icons-material/Logout";
+import "./App.scss";
+function Header() {
+  return (
+    <div className="header">
+      <Navbar bg="light" variant="light" expand="lg">
+        <Container>
+          {/* <href> */} <h1 className="logo">TINY</h1>
+          {/* </href> */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#Outstanding">Nổi Bật</Nav.Link>
+              <NavDropdown title="Smartphone" id="Smartphone-dropdown">
+                <NavDropdown.Item href="#Samsung">Samsung</NavDropdown.Item>
+                <NavDropdown.Item href="#Iphone">iPhone</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Phone Accessories" id="PhonAcc-dropdown">
+                <NavDropdown.Item href="#earphone">Earphone</NavDropdown.Item>
+                <NavDropdown.Item href="#case">Phone Case</NavDropdown.Item>
+              </NavDropdown>
+              <form action="">
+                <input type="text" placeholder="Search" />
+                <SearchIcon type="submit" id="search-icon" />
+              </form>
+            </Nav>
+          </Navbar.Collapse>
+          <div className="group-icon">
+            <PersonIcon />
+            <ShoppingCartIcon />
+            <LogoutIcon />
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
