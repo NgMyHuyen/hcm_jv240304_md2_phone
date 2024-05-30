3059b3e5aaa952bd5981c9f3ce957dadc2d14d89
Your Name <you@example.com>
2024-05-30 17:30:55 +0700

    check merge feature/add_delete

--- src/store/Slices/store.js
+++ src/store/Slices/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
-import { authActions } from "./User.slice";
+import signUpSlice from "../Slices/signUpSlice";
+import authSlice from "../Slices/userSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    auth: authActions,
+    auth: authSlice,
+    signUp: signUpSlice,
   },
 });
 export default store;
